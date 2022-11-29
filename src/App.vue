d6bc092aadbfcab8a7e19c6e4fe33b3a91a5196e
Vadim Gerasimenko <vadimgerasimenko101@gmail.com>
2022-11-29 17:36:48 +0300

    add styles for nav links

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,22 @@ nav {
   width: 100%;
   font-size: 1.2rem;
   text-align: center;
-  margin-top: 2rem;
+  margin: 0.8rem auto;
+}
+
+nav a {
+  display: inline-block;
+
+  width: 6rem;
+  height: 2rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+
+  border-radius: 15px;
+  background-color: rgb(190, 190, 190);
+}
+
+nav a:hover {
+  box-shadow: 2px 2px 4px black;
 }
 </style>
